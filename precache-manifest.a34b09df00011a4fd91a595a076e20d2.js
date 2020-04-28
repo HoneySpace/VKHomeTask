27470064ef64adb00872493370ba2c482fa516fd
Honey_space <ritmac@mail.ru>
2020-04-28 20:25:38 +0700

    Lounch

---
+++ precache-manifest.a34b09df00011a4fd91a595a076e20d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82858a17971f958f92cf85eb9965a83a",
+    "url": "/VKHomeTask/index.html"
+  },
+  {
+    "revision": "eeb223bdc07125bb0d25",
+    "url": "/VKHomeTask/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ba16fe4a1f88fbbc6537",
+    "url": "/VKHomeTask/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "eeb223bdc07125bb0d25",
+    "url": "/VKHomeTask/static/js/2.55414c2c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/VKHomeTask/static/js/2.55414c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba16fe4a1f88fbbc6537",
+    "url": "/VKHomeTask/static/js/main.2d225a2c.chunk.js"
+  },
+  {
+    "revision": "1421817bbc5071d98896",
+    "url": "/VKHomeTask/static/js/runtime-main.6cb6e552.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VKHomeTask/static/media/persik.4e1ec840.png"
+  }
+]);
